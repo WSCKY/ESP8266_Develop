63d0d41328228a65b5f0790c49ce2fb6c521b5b6
kyChu <hello.kychu@gmail.com>
2017-09-06 19:44:36 +0800

    add a simple test code

---
+++ AppTest/user/user_main.c
@@ -0,0 +1,159 @@
+/*
+ * ESPRSSIF MIT License
+ *
+ * Copyright (c) 2015 <ESPRESSIF SYSTEMS (SHANGHAI) PTE LTD>
+ *
+ * Permission is hereby granted for use on ESPRESSIF SYSTEMS ESP8266 only, in which case,
+ * it is free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+#include "esp_common.h"
+#include "user_config.h"
+//#include "uart.h"
+
+/******************************************************************************
+ * FunctionName : user_rf_cal_sector_set
+ * Description  : SDK just reversed 4 sectors, used for rf init data and paramters.
+ *                We add this function to force users to set rf cal sector, since
+ *                we don't know which sector is free in user's application.
+ *                sector map for last several sectors : ABCCC
+ *                A : rf cal
+ *                B : rf init data
+ *                C : sdk parameters
+ * Parameters   : none
+ * Returns      : rf cal sector
+*******************************************************************************/
+uint32 user_rf_cal_sector_set(void)
+{
+    flash_size_map size_map = system_get_flash_size_map();
+    uint32 rf_cal_sec = 0;
+
+    switch (size_map) {
+        case FLASH_SIZE_4M_MAP_256_256:
+            rf_cal_sec = 128 - 5;
+            break;
+
+        case FLASH_SIZE_8M_MAP_512_512:
+            rf_cal_sec = 256 - 5;
+            break;
+
+        case FLASH_SIZE_16M_MAP_512_512:
+        case FLASH_SIZE_16M_MAP_1024_1024:
+            rf_cal_sec = 512 - 5;
+            break;
+
+        case FLASH_SIZE_32M_MAP_512_512:
+        case FLASH_SIZE_32M_MAP_1024_1024:
+            rf_cal_sec = 1024 - 5;
+            break;
+        case FLASH_SIZE_64M_MAP_1024_1024:
+            rf_cal_sec = 2048 - 5;
+            break;
+        case FLASH_SIZE_128M_MAP_1024_1024:
+            rf_cal_sec = 4096 - 5;
+            break;
+        default:
+            rf_cal_sec = 0;
+            break;
+    }
+
+    return rf_cal_sec;
+}
+/*
+LOCAL void uart0_rx_intr_handler(void *para)
+{
+}*/
+
+void task2(void *pv)
+{
+    printf("Hello, Welcome to task2!\n");
+
+/*    struct station_info *station = wifi_softap_get_station_info();
+    while(station) {
+        printf("bssid: %s, ip: %s\n", MAC2STR(station->bssid), IP2STR(&station->ip));
+        station = STAILQ_NEXT(station, next);
+    }
+    wifi_softap_free_station_info();*/
+    for(;;);
+    vTaskDelete(NULL);
+}
+
+/******************************************************************************
+ * FunctionName : user_init
+ * Description  : entry of user application, init user function here
+ * Parameters   : none
+ * Returns      : none
+*******************************************************************************/
+void user_init(void)
+{
+/*    UART_WaitTxFifoEmpty(UART0);
+
+    UART_ConfigTypeDef uart_config;
+    uart_config.baud_rate    = BIT_RATE_74880;
+    uart_config.data_bits     = UART_WordLength_8b;
+    uart_config.parity          = USART_Parity_None;
+    uart_config.stop_bits     = USART_StopBits_1;
+    uart_config.flow_ctrl      = USART_HardwareFlowControl_None;
+    uart_config.UART_RxFlowThresh = 120;
+    uart_config.UART_InverseMask = UART_None_Inverse;
+    UART_ParamConfig(UART0, &uart_config);
+
+    UART_IntrConfTypeDef uart_intr;
+    uart_intr.UART_IntrEnMask = UART_RXFIFO_TOUT_INT_ENA | UART_FRM_ERR_INT_ENA | UART_RXFIFO_FULL_INT_ENA | UART_TXFIFO_EMPTY_INT_ENA;
+    uart_intr.UART_RX_FifoFullIntrThresh = 10;
+    uart_intr.UART_RX_TimeOutIntrThresh = 2;
+    uart_intr.UART_TX_FifoEmptyIntrThresh = 20;
+    UART_IntrConfig(UART0, &uart_intr);
+
+    UART_SetPrintPort(UART0);
+    UART_intr_handler_register(uart0_rx_intr_handler, NULL);
+    ETS_UART_INTR_ENABLE();
+************  With Bug  ************
+*/
+/*    printf("SDK version:%s\n", system_get_sdk_version());
+    printf("ESP8266 chip ID:0x%x\n", system_get_chip_id());*/
+
+    /* station mode */
+    wifi_set_opmode(SOFTAP_MODE);
+
+    struct softap_config *config = (struct softap_config *)zalloc(sizeof(struct softap_config)); //initialization.
+    wifi_softap_get_config(config);
+    sprintf(config->ssid, MY_AP_SSID);
+    sprintf(config->password, MY_AP_PASSWD);
+    config->authmode = AUTH_WPA_WPA2_PSK;
+    config->ssid_len = 0;
+    config->max_connection = 4;
+    wifi_softap_set_config(config);
+    free(config);
+
+    xTaskCreate(task2, "task2", 256, NULL, 2, NULL);
+
+    wifi_softap_dhcps_stop();                    // disable soft-AP DHCP server
+    struct ip_info info;
+    IP4_ADDR(&info.ip, 192, 168, 5, 1);             // set IP
+    IP4_ADDR(&info.gw, 192, 168, 5, 1);             // set gateway
+    IP4_ADDR(&info.netmask, 255, 255, 255, 0);      // set netmask
+    wifi_set_ip_info(SOFTAP_IF, &info);
+
+    struct dhcps_lease dhcp_lease;
+    IP4_ADDR(&dhcp_lease.start_ip, 192, 168, 5, 100);
+    IP4_ADDR(&dhcp_lease.end_ip, 192, 168, 5, 105);
+    wifi_softap_set_dhcps_lease(&dhcp_lease);
+    wifi_softap_dhcps_start();                     // enable soft-AP DHCP server
+}
+
