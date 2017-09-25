71bf342d6314893455bd91b93014a39e3ddd84f9
kyChu <hello.kychu@gmail.com>
2017-09-25 18:07:35 +0800

    prepare for xPartner wifi module development

---
+++ xPartnerProj/include/tftp_data.h
@@ -0,0 +1,22 @@
+#ifndef __FSDATA_H
+#define __FSDATA_H
+
+static const char Testdata[] = "The ESP8266EX offers a complete and self-contained Wi-Fi SoC solution. "
+"With low power consumption, a compact design and high stability, it caters to the needs of its users.\r\n"
+"It can be used to host applications or to offload Wi-Fi network functions from other"
+"application processors. When the ESP8266 hosts an application, as the only processor in"
+"the device, it boots up directly from an external flash. It has an built-in, high-speed cache"
+"which improves the performance of the system and optimizes the memory. Alternatively,"
+"when the ESP8266EX is used as a Wi-Fi adapter, wireless internet access can be added to"
+"any microcontroller-based device through the SPI/SDIO interface or the I2C/UART"
+"interface, and thus provide the users with a simple Wi-Fi solution.\r\n"
+"Furthermore, the ESP8266EX offers a high level of on-chip integration. It integrates the"
+"antenna switch, RF balun, power amplifier, low-noise receive amplifier, filters, and power"
+"management modules. It requires minimal external circuitry, and the entire solution,"
+"including the front-end module, is designed to occupy minimal PCB space.\r\n"
+"The ESP8266EX also integrates an enhanced version of the 32-bit processor of Tensilica’s"
+"L106 Diamond series, with on-chip SRAM. The ESP8266EX is often integrated with"
+"external sensors and other application-specific devices through its GPIOs. The SDK files"
+"provide examples of the software of the related applications.";
+
+#endif /* __FSDATA_H */
