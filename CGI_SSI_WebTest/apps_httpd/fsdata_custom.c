003d6c8296590bc9f2f2da86fcef11eb5b9c3513
kyChu <hello.kychu@gmail.com>
2017-10-09 17:54:12 +0800

    realize custom fs data operate interface

---
+++ CGI_SSI_WebTest/apps_httpd/fsdata_custom.c
@@ -0,0 +1,53 @@
+#include "fs.h"
+#include "lwip/def.h"
+#include "fsdata.h"
+
+#define _HTML_INDEX_LEN          1872
+#define _HTML_INDEX_ADDR         0x81000
+#define _HTML_404_LEN            785
+#define _HTML_404_ADDR           0x82000
+#define _HTML_LOGO_LEN           4030
+#define _HTML_LOGO_ADDR          0x83000
+#define _HTML_UPLOAD_LEN         1559
+#define _HTML_UPLOAD_ADDR        0x84000
+#define _HTML_PNG_LEN            54867
+#define _HTML_PNG_ADDR           0x85000
+
+struct file_opt {
+	const unsigned char *name;
+	const unsigned int  len;
+	const unsigned int  addr;
+};
+
+const struct file_opt custom_files[] = {
+	{"/index.html", _HTML_INDEX_LEN, _HTML_INDEX_ADDR},
+	{"/img/header.png", _HTML_PNG_LEN, _HTML_PNG_ADDR},
+	{"/img/logo.ico", _HTML_LOGO_LEN, _HTML_LOGO_ADDR},
+	{"/kyChu/login.cgi", _HTML_UPLOAD_LEN, _HTML_UPLOAD_ADDR},
+	{"/404.html", _HTML_404_LEN, _HTML_404_ADDR}
+};
+
+#define NUM_CUSTOM_FILES (sizeof(custom_files) / sizeof(struct file_opt))
+
+int fs_open_custom(struct fs_file *file, const char *name) {
+	int i = NUM_CUSTOM_FILES - 1;
+//	printf("exp file: %s, files = %d.\n", name, NUM_CUSTOM_FILES);
+	for(; i >= 0; -- i) {
+		if(!strcmp(name, custom_files[i])) {
+			printf("find exp file: %s.\n", custom_files[i]);
+			break;
+		}
+//		printf("find file: %s.\n", custom_files[i]);
+	}
+	return 0;
+}
+
+int fs_read_custom(struct fs_file *file, char *buffer, int count)
+{
+	printf("read custom data.\n");
+}
+
+void fs_close_custom(struct fs_file *file)
+{
+	printf("close custom file.\n");
+}
