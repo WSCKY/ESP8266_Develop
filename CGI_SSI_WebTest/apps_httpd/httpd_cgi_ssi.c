97fe47bc6c808a37da9025f7dd2fca1744ee79b2
kyChu <hello.kychu@gmail.com>
2017-10-10 14:39:58 +0800

    add cgi handler(form method should be get)

--- CGI_SSI_WebTest/apps_httpd/httpd_cgi_ssi.c
+++ CGI_SSI_WebTest/apps_httpd/httpd_cgi_ssi.c
@@ -1,12 +1,17 @@
 #include "httpd.h"
 #include "lwip/tcp.h"
 
+const char *Print_Handler(int iIndex, int iNumParams, char *pcParam[], char *pcValue[]);
+
 const char* TAGS[] = {"echo", "here"};
 static char* display_1 = " echo: hi, world! - 0<br />";
 static char* display_2 = " here: hi, world! - 0<br />";
 
 static uint8_t req_cnt_1 = 0;
 static uint8_t req_cnt_2 = 0;
+
+const tCGI PRINT_CGI = {"/kyChu/print.cgi", Print_Handler};
+tCGI CGI_TAB[1];
 
 u16_t StringHandler(int iIndex, char *pcInsert, int iInsertLen)
 {
@@ -32,7 +37,21 @@ u16_t StringHandler(int iIndex, char *pcInsert, int iInsertLen)
 	return 0;
 }
 
+const char *Print_Handler(int iIndex, int iNumParams, char *pcParam[], char *pcValue[])
+{
+	int i = 0;
+	if(iIndex == 0) {
+		printf("CGI > got param num: %d.\n", iNumParams);
+		for(i == 0; i < iNumParams; i ++) {
+			printf("param is: %s, Vaule is %s.\n", pcParam[i], pcValue[i]);
+		}
+	}
+	return "/index.shtml";
+}
+
 void Httpd_cgi_ssi_init(void)
 {
 	http_set_ssi_handler(StringHandler, (char const **)TAGS, 2);
+	CGI_TAB[0] = PRINT_CGI;
+	http_set_cgi_handlers(CGI_TAB, 1);
 }
