23fb4d43dacece9ac0e3d1079a02ab663746dbc7
kyChu <hello.kychu@gmail.com>
2017-10-10 15:59:41 +0800

    implement specific functions and add any code for test.

--- CGI_SSI_WebTest/apps_httpd/httpd_cgi_ssi.c
+++ CGI_SSI_WebTest/apps_httpd/httpd_cgi_ssi.c
@@ -49,6 +49,39 @@ const char *Print_Handler(int iIndex, int iNumParams, char *pcParam[], char *pcV
 	return "/index.shtml";
 }
 
+err_t httpd_post_begin(void *connection, const char *uri, const char *http_request,
+                       u16_t http_request_len, int content_len, char *response_uri,
+                       u16_t response_uri_len, u8_t *post_auto_wnd) {
+//	printf("a POST request has been received.\n");
+//	printf("uri is %s.\n", uri);
+	return ERR_OK;
+}
+
+char cache[20];
+err_t httpd_post_receive_data(void *connection, struct pbuf *p) {
+//	printf("each pbuf of data that has been received for a POST.\n");
+//	printf("tot_len = %d, len = %d.\n", p->tot_len, p->len);
+//	printf("payload is %s.\n", p->payload);
+	int i = 0, j = 0;
+	while(i < p->len && ((char *)p->payload)[i] != '=') i ++;
+	do {
+		i ++;
+		cache[j ++] = ((char *)p->payload)[i];
+	} while(i < p->len && ((char *)p->payload)[i] != '&');
+	cache[j - 1] = 0;
+	printf("%s\n", cache);
+	return ERR_OK;
+}
+
+static const char* rsp_uri = "/index.shtml";
+void httpd_post_finished(void *connection, char *response_uri, u16_t response_uri_len) {
+	uint32_t i = 0;
+//	printf("all data is received or the connection is closed.\n");
+	for(; i < 12; i ++) {
+		response_uri[i] = rsp_uri[i];
+	}
+}
+
 void Httpd_cgi_ssi_init(void)
 {
 	http_set_ssi_handler(StringHandler, (char const **)TAGS, 2);
