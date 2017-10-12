e2abf7388996480ddc8f69f04cc25a2ca6f6f172
kyChu <hello.kychu@gmail.com>
2017-10-12 19:52:50 +0800

    add simple code to parse filename from content

--- CGI_SSI_WebTest/apps_httpd/httpd_cgi_ssi.c
+++ CGI_SSI_WebTest/apps_httpd/httpd_cgi_ssi.c
@@ -52,6 +52,8 @@ const char *Print_Handler(int iIndex, int iNumParams, char *pcParam[], char *pcV
 static uint8_t UpgradeCmd = 0;
 
 static char req_uri[20];
+#define FILE_NAME_MAX_LENGTH 40
+static char filename[FILE_NAME_MAX_LENGTH + 1];
 static const char* default_uri = "/index.shtml";
 err_t httpd_post_begin(void *connection, const char *uri, const char *http_request,
                        u16_t http_request_len, int content_len, char *response_uri,
@@ -85,6 +87,25 @@ err_t httpd_post_receive_data(void *connection, struct pbuf *p) {
 		memcpy(req_uri, default_uri, strlen(default_uri));
 		req_uri[strlen(default_uri)] = 0;
 	} else if((!strcmp(req_uri, "/upgrade/wifi.cgi")) || (!strcmp(req_uri, "/upgrade/fc.cgi"))) {
+		uint32_t FileNameOffset = 0;
+		uint32_t i = 0;
+		while(++ i < p->len) {
+			if(((char *)p->payload)[i] == 'f') {
+				if(strncmp(&((char *)p->payload)[i], "filename=", 9) == 0) {
+					FileNameOffset = i + 10;
+					break;
+				}
+			}
+		}
+		if(FileNameOffset) {
+			i = 0;
+			while(((char *)p->payload)[FileNameOffset] != '\"' && i < FILE_NAME_MAX_LENGTH) {
+				filename[i ++] = ((char *)p->payload)[FileNameOffset];
+				FileNameOffset ++;
+			}
+			filename[i] = 0;
+			printf("file name is %s.\n", filename);
+		}
 		memcpy(req_uri, default_uri, strlen(default_uri));//jump to default page(index page).
 		req_uri[strlen(default_uri)] = 0;
 	}
