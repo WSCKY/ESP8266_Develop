46c092a6743b18df0e16070c5451d97172411514
kyChu <hello.kychu@gmail.com>
2018-01-02 16:28:33 +0800

    update uart configuration to communicate with receiver

--- Futaba_EX_Wifi/user/uart.c
+++ Futaba_EX_Wifi/user/uart.c
@@ -400,13 +400,13 @@ uart_init_new(void)
     UART_WaitTxFifoEmpty(UART1);
 
     UART_ConfigTypeDef uart_config;
-    uart_config.baud_rate         = BIT_RATE_115200;
+    uart_config.baud_rate         = 100000;//BIT_RATE_115200;
     uart_config.data_bits         = UART_WordLength_8b;
-    uart_config.parity            = USART_Parity_None;
-    uart_config.stop_bits         = USART_StopBits_1;
+    uart_config.parity            = USART_Parity_Even;//USART_Parity_None;
+    uart_config.stop_bits         = USART_StopBits_2;//USART_StopBits_1;
     uart_config.flow_ctrl         = USART_HardwareFlowControl_None;
     uart_config.UART_RxFlowThresh = 120;
-    uart_config.UART_InverseMask  = UART_None_Inverse;
+    uart_config.UART_InverseMask  = UART_Rxd_Inverse;//UART_None_Inverse;
     UART_ParamConfig(UART0, &uart_config);
 
     uart_config.baud_rate         = BIT_RATE_74880;
